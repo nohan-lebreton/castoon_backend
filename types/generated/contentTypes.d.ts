b8954dda0d7b744474499b961b1ea5a865f0377e
Nohan Leb <lebreton.nohan@gmail.com>
2025-06-19 00:53:18 +0200

    nohan

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -1104,7 +1104,14 @@ export interface PluginUsersPermissionsUser
       Schema.Attribute.SetMinMaxLength<{
         minLength: 6;
       }>;
-    pin: Schema.Attribute.Password;
+    pin: Schema.Attribute.BigInteger &
+      Schema.Attribute.SetMinMax<
+        {
+          max: '9999';
+        },
+        string
+      > &
+      Schema.Attribute.DefaultTo<'0000'>;
     provider: Schema.Attribute.String;
     publishedAt: Schema.Attribute.DateTime;
     resetPasswordToken: Schema.Attribute.String & Schema.Attribute.Private;
